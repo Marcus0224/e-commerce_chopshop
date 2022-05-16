e21c88e092abf7992d3aaf2b7aa3c8b12ff0a97a
Marcus0224 <marcstlouis04@gmail.com>
2022-05-16 19:02:02 -0400

    added port to server.js & auth factors

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const routes = require("./Develop/routes");
+const sequelize = require("./Develop/config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Now listening 👂 on port ${PORT}!`);
+  });
+});
